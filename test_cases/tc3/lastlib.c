08d2d3fdc60b4534782869471d6fd254455d9386
rupeshkoushik07 <koushik.rupesh07@gmail.com>
2026-06-17 17:53:47 +0200

    remove old test cases

--- test_cases/tc3/lastlib.c
+++
@@ -1,8 +0,0 @@
-// lastlib.c - Contains the actual mkdir syscall
-#include <sys/stat.h>
-#include <sys/types.h>
-
-// Function that actually performs the mkdir syscall
-int mkdir_syscall(const char* path) {
-    return mkdir(path, 0755);
-}
