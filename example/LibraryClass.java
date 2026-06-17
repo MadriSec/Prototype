10f49ad65a71202072f3650818cf7b7ca80042d0
rupeshkoushik07 <koushik.rupesh07@gmail.com>
2026-06-17 15:06:31 +0200

    remove unused dir

--- example/LibraryClass.java
+++
@@ -1,7 +0,0 @@
-package com.example.library;
-
-public class LibraryClass {
-    public String getMessage() {
-        return "Hello from Library!";
-    }
-}
--- example/com/example/library/LibraryClass.java
+++
@@ -1,9 +0,0 @@
-package com.example.library;
-
-import com.example.util.HelperClass;
-
-public class LibraryClass {
-    public String getMessage() {
-        return "Hello from Library! " + new HelperClass().helperMessage();
-    }
-}
