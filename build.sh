b3d66c29aa5d68299d49e484fdae29e1521449e6
rupeshkoushik07 <koushik.rupesh07@gmail.com>
2026-06-17 17:06:10 +0200

    Remove sudo from build script

--- build.sh
+++ build.sh
@@ -6,12 +6,12 @@ set -e  # Exit immediately if a command exits with a non-zero status
 # export MAVEN_OPTS="--add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED --add-opens=java.base/java.io=ALL-UNNAMED --add-opens=java.base/java.util=ALL-UNNAMED --add-opens=java.base/java.lang.invoke=ALL-UNNAMED --add-opens=java.base/java.util.concurrent=ALL-UNNAMED"
 
 echo "Cleaning and compiling..."
-sudo mvn clean compile
+mvn clean compile
 
 echo "Packaging the project..."
-sudo mvn clean package
+mvn clean package -DskipTests
 
 echo "Copying dependencies to target/deps..."
-sudo mvn dependency:copy-dependencies -DoutputDirectory=target/deps
+mvn dependency:copy-dependencies -DoutputDirectory=target/deps
 
 
